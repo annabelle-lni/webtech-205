16033539302d306f502eb611b625de3f944d21b9
Pierre-Louis245 <132985110+Pierre-Louis245@users.noreply.github.com>
2025-10-08 09:52:13 +0200

    feat : fin du lab2, on a trois chemins accessibles via l'url, /, /hello et /hello?name

--- handles.js
+++ handles.js
@@ -1,20 +1,41 @@
 const url = require('url')
 const qs = require('querystring')
+const aboutContent = require('./content/about.json')
 
 module.exports = {
     serverHandle: function (req, res) {
         const route = url.parse(req.url)
         const path = route.pathname 
         const params = qs.parse(route.query)
+        
+        if(path === '/'){
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write('<h1>Bienvenue</h1>')
+            res.write(`<p>Entrer /hello suivi du nom Pierre-Louis pour pouvoir acceder au "site"</p>`)
+            res.end()
+        }else if (path === '/hello') {
+            if(!params.name){
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write('Bonjour monsieur anonyme')
+            }else if(params.name === 'Pierre-Louis'){
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write('Hello Pierre-Louis ')
+                res.write('Je suis etudiant a l ece')
+            }else {
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write(`<p>Hello ${params.name} </p>`)
+            }
+            res.end()
 
-        res.writeHead(200, {'Content-Type': 'text/plain'})
+        } else if(path === '/about'){
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.write(JSON.stringify(aboutContent))
+            res.end()
 
-        if (path === '/hello' && 'name' in params) {
-            res.write('Hello ' + params['name'])
-        } else {
-            res.write('Hello anonymous')
+        }else {
+            res.writeHead(404)
+            res.write('<h1>Page non trouvée</h1>')
+            res.end()
         }
-  
-    res.end()
-}
+    }
 }
